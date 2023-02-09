ec1a7711d9afe3e13350449a6993d6ad4c7c832d
rgsad <rsad3262@gmail.com>
2023-02-10 00:47:16 +0300

    refactor TruePrinter & FalsePrinter

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
-        if (false) {
+        if (true) {
             System.out.println(true);
         } else {
-            System.out.println(false);
+            System.out.println(true);
         }
     }
 }
