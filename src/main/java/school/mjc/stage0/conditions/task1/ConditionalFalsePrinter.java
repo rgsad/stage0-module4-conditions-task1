61dbe662f1caa84a732b64855a5304724c3be991
rgsad <rsad3262@gmail.com>
2023-02-10 00:27:49 +0300

    start task

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int first = 1;
+        int second = 2;
+
+        if (first > second) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int first = 0;
+        int second = 1;
+
+        if (second > first) {
+            System.out.println(true);
+        }
     }
 }
